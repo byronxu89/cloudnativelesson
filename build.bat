a11cbc977c2a19c3473429955d39c6c3a0cb459b
xubh <aussiebruce@sina.com.cn>
2021-11-29 00:22:57 +0800

    delete old version

--- build.bat
+++
@@ -1,6 +0,0 @@
-SET CGO_ENABLED=0
-SET GOOS=linux
-set GOARCH=amd64
-:: set GOARM=7
-
-go build main.go
--- test.sh
+++
@@ -1,3 +0,0 @@
-docker pull acleverguy/simplehttpserver:unchanged
-
-docker run -d -p 8090:8090 acleverguy/simplehttpserver:unchanged
